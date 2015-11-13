10ac903bbd20864ce8b5238271db2e778bc14807
Simon Fayer <sf105@ic.ac.uk>
2015-11-13 08:32:21 +0000

    Update bootstrap for CentOS6.7.

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,32 +5,32 @@
 set -e # Stop on errors
 mkdir -p boot
 
-# CentOS 6.6 (LZMA)
-if [ ! -e boot/centos66_vmlinuz ]; then
-  wget -O boot/centos66_vmlinuz \
-    "http://www.mirrorservice.org/sites/mirror.centos.org/6.6/os/x86_64/images/pxeboot/vmlinuz"
+# CentOS 6.7 (LZMA)
+if [ ! -e boot/centos67_vmlinuz ]; then
+  wget -O boot/centos67_vmlinuz \
+    "http://www.mirrorservice.org/sites/mirror.centos.org/6.7/os/x86_64/images/pxeboot/vmlinuz"
 fi
-if [ ! -e boot/centos66_initrd.img ]; then
-  wget -O boot/centos66_initrd.img.lzma \
-    "http://www.mirrorservice.org/sites/mirror.centos.org/6.6/os/x86_64/images/pxeboot/initrd.img"
-  echo "Recompressing CentOS 6.6 initrd..." >&2
-  unlzma boot/centos66_initrd.img.lzma
-  gzip -9 boot/centos66_initrd.img
-  mv boot/centos66_initrd.img.gz boot/centos66_initrd.img
+if [ ! -e boot/centos67_initrd.img ]; then
+  wget -O boot/centos67_initrd.img.lzma \
+    "http://www.mirrorservice.org/sites/mirror.centos.org/6.7/os/x86_64/images/pxeboot/initrd.img"
+  echo "Recompressing CentOS 6.7 initrd..." >&2
+  unlzma boot/centos67_initrd.img.lzma
+  gzip -9 boot/centos67_initrd.img
+  mv boot/centos67_initrd.img.gz boot/centos67_initrd.img
 fi
 
-# Scientific Linux 6.6 (LZMA)
-if [ ! -e boot/sl66_vmlinuz ]; then
-  wget -O boot/sl66_vmlinuz \
+# Scientific Linux 6.7 (LZMA)
+if [ ! -e boot/sl67_vmlinuz ]; then
+  wget -O boot/sl67_vmlinuz \
     "http://www.mirrorservice.org/sites/ftp.scientificlinux.org/linux/scientific/6/x86_64/os/images/pxeboot/vmlinuz"
 fi
-if [ ! -e boot/sl66_initrd.img ]; then
-  wget -O boot/sl66_initrd.img.lzma \
+if [ ! -e boot/sl67_initrd.img ]; then
+  wget -O boot/sl67_initrd.img.lzma \
     "http://www.mirrorservice.org/sites/ftp.scientificlinux.org/linux/scientific/6/x86_64/os/images/pxeboot/initrd.img"
-  echo "Recompressing Scientific Linux 6.6 initrd..." >&2
-  unlzma boot/sl66_initrd.img.lzma
-  gzip -9 boot/sl66_initrd.img
-  mv boot/sl66_initrd.img.gz boot/sl66_initrd.img
+  echo "Recompressing Scientific Linux 6.7 initrd..." >&2
+  unlzma boot/sl67_initrd.img.lzma
+  gzip -9 boot/sl67_initrd.img
+  mv boot/sl67_initrd.img.gz boot/sl67_initrd.img
 fi
 
 
@@ -66,10 +66,11 @@ echo "Verifying checksums..." >&2
 sha1sum -c << EOF
 957730e883af19dde3a615c542f3ec3751c18b03  boot/centos511_initrd.img
 c65aa075bd344099e0093bca21a481764f495c3a  boot/centos511_vmlinuz
-ecfb640c3ef9f04fa46f892300e850ef0203aaaf  boot/centos66_initrd.img
-ef0d91c733e2422c523b50b3718f7cb50bb0f023  boot/centos66_vmlinuz
-3b079e955553e5f1f766877ff87b9ef2052f1791  boot/sl66_initrd.img
-c682c7cac71f233bcb816c5ae68de8646a9f41e3  boot/sl66_vmlinuz
+92909fef6a8e401f5d467867d3c8a58ac5e05595  boot/centos67_initrd.img
+f7e7bcb4f46bb73fe2daba90dc7a564072cfb43f  boot/centos67_vmlinuz
+b58beec5ad85d735980fd435a2a74b174ca10e65  boot/sl67_initrd.img
+9b386b46401cb96a1a6860b823b3517f1afbbaee  boot/sl67_vmlinuz
+
 7165db4aedb5dcbb1436e9597c910508b2a002e3  boot/fedora20_initrd.img
 fb56dd066b7b2017b634b09f16736ee23f1e32cd  boot/fedora20_vmlinuz
 EOF
